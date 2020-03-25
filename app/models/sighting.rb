09ff3c3f20b66a30a49d8aa1226d24358a5ccf0c
James Lenahan <james.k.lenahan@gmail.com>
2020-03-25 19:35:30 +0000

    Done.

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,9 @@
-class Sighting < ApplicationRecord
-  belongs_to :bird
-  belongs_to :location
+class SightingsController < ApplicationController
+  def show
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at])
+  end
 end
