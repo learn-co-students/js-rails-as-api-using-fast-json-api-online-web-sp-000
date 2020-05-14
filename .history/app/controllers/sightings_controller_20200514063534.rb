ae08c1abb8097ef72842440ca148241f88844e81
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-05-14 06:44:30 -0400

    Done.

--- .history/app/controllers/sightings_controller_20200514063534.rb
+++ .history/app/controllers/sightings_controller_20200514063534.rb
@@ -2,11 +2,11 @@ class SightingsController < ApplicationController
   def index
   sightings = Sighting.all
   render json: SightingSerializer.new(sightings)
-  end
+end
 
   def show
     sighting = Sighting.find(params[:id])
     # render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
-    render json: SightingSerializer.new(sighting, options)
+    render json: SightingSerializer.new(sighting)
   end
 end
